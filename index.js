ed48974ecb5590e56970eaaa05237f61418c8c34
felix mwangi <fmwauramwangi@gmail.com>
2025-01-08 17:07:41 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your code here
+// define the brekfast class
+
+class Breakfast {
+    constructor(food, drink) {
+        this.food = food;
+        this.drink = drink;
+    }
+}
+
+//define the lunch class
+class Lunch {
+    constructor (salad, soup, drink) {
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+    }
+}
+
+//define the dinner class
+
+class Dinner {
+    #dessert; //private field for dessert
+    constructor(salad, soup, entree, dessert){
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree;
+        this.#dessert = dessert;
+    }
+}
